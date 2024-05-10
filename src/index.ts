ca91aae37b60a7dccc0582a16feebe1bfc1af78d
LinJieLinLin <993353454@qq.com>
2024-05-10 21:44:32 +0800

    feat: up color rule

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import baseCssList from './baseCssRule'
  */
 export interface IOption {
   unit?: string
+  /** weapp：weixin microApp */
   type?: 'weapp'
 }
 
@@ -20,84 +21,97 @@ function presetLj(options: IOption = {}): Preset {
 }
 const getFnRuleList = (options: IOption = {}) => {
   const { unit = 'px' } = options
-  const res: Rule[] = []
-  // 文字溢出隐藏
-  res.push([
-    /^text-line-([\d]+)$/,
-    ([_, num]) => ({
-      'word-break': 'break-all',
-      overflow: 'hidden',
-      'text-overflow': 'ellipsis',
-      display: '-webkit-box',
-      '-webkit-box-orient': 'vertical',
-      /*! autoprefixer: off */
-      '-webkit-line-clamp': `${num}`,
-    }),
-  ])
-
-  // 字号设置
-  res.push([/^fs-([\d]+)$/, ([_, num]) => ({ 'font-size': `${num}${unit}` })])
-  // 同时设置宽高
-  res.push([
-    /^wh-([\d]+)$/,
-    ([_, num]) => ({ width: `${num}${unit}`, height: `${num}${unit}` }),
-  ])
-  // 文字颜色
-  res.push([
-    /^c-([\d|abcdef]{1,3})$/,
-    ([_, num], { rawSelector }) => {
-      let count = 1
-      count = 6 / num.length
-      return `${e(rawSelector)}{color:#${num.repeat(count)}}`
-    },
-  ])
-  // 背景颜色
-  res.push([
-    /^bg-([\d|\w]{1,2})$/,
-    ([_, num], { rawSelector }) => {
-      let count = 1
-      count = 6 / num.length
-      return `${e(rawSelector)}{background-color:#${num.repeat(count)}}`
-    },
-  ])
-  res.push([
-    /^c-b/,
-    ([_]) => {
-      return `.c-b::after{content: "";display: block;clear: both;}`
-    },
-  ])
-  // border-radius
-  res.push([
-    /^br-([\d]+)$/,
-    ([_, num]) => ({ 'border-radius': `${num}${unit}` }),
-  ])
-  // flex
-  res.push([/^flex-{0,1}([\d]+)$/, ([_, num]) => ({ flex: `${num}` })])
-  // position,pos-
-  res.push([
-    /^p-([sfard]{1})$/,
-    ([_, type], { rawSelector }) => {
-      const selector = e(rawSelector)
-      const keyObj = {
-        s: 'sticky',
-        f: 'fixed',
-        a: 'absolute',
-        r: 'relative',
-        d: 'static',
-      }
-      return `${selector}{position:${keyObj[type]};}`
-    },
-  ])
-  // rotate角度
-  res.push([
-    /^rot-([\d]+)$/,
-    ([_, num], { rawSelector }) => {
-      const selector = e(rawSelector)
-      return `${selector}{transform:rotate(${num}deg)}`
-    },
-  ])
-  // margin padding 重写 for 小程序
+  const res: Rule[] = [
+    // text overflow hide with ...
+    [
+      /^text-line-([\d]+)$/,
+      ([_, num]) => ({
+        'word-break': 'break-all',
+        overflow: 'hidden',
+        'text-overflow': 'ellipsis',
+        display: '-webkit-box',
+        '-webkit-box-orient': 'vertical',
+        /*! autoprefixer: off */
+        '-webkit-line-clamp': `${num}`,
+      }),
+    ],
+    // font-size
+    [/^fs-([\d]+)$/, ([_, num]) => ({ 'font-size': `${num}${unit}` })],
+    // width height
+    [
+      /^wh-([\d]+)$/,
+      ([_, num]) => ({ width: `${num}${unit}`, height: `${num}${unit}` }),
+    ],
+    // text color short,eg: c-3 {color: #333333;}
+    [
+      /^c-([\d|abcdef]{1,3})$/,
+      ([_, num], { rawSelector }) => {
+        let count = 1
+        count = 6 / num.length
+        return `${e(rawSelector)}{color:#${num.repeat(count)}}`
+      },
+    ],
+    // text color
+    [
+      /^c-[\d|abcdef]{6,8})$/,
+      ([_, v], { rawSelector }) => {
+        return `${e(rawSelector)}{color:#${v}}`
+      },
+    ],
+    // background color,eg: bg-3 {background-color: #333333;}
+    [
+      /^bg-([\d|abcdef]{1,3})$/,
+      ([_, num], { rawSelector }) => {
+        let count = 1
+        count = 6 / num.length
+        return `${e(rawSelector)}{background-color:#${num.repeat(count)}}`
+      },
+    ],
+    // background color
+    [
+      /^bg-([\d|abcdef]{6,8})$/,
+      ([_, v], { rawSelector }) => {
+        return `${e(rawSelector)}{background-color:#${v}}`
+      },
+    ],
+    // clear float add at father container
+    [
+      /^f-c/,
+      ([_]) => {
+        return `.f-c::after{content: "";display: block;clear: both;}`
+      },
+    ],
+    // border-radius
+    [/^br-([\d]+)$/, ([_, num]) => ({ 'border-radius': `${num}${unit}` })],
+    // flex
+    [/^flex-{0,1}([\d]+)$/, ([_, num]) => ({ flex: `${num}` })],
+    // position,as pos-
+    [
+      /^p-([sfard]{1})$/,
+      ([_, type], { rawSelector }) => {
+        const selector = e(rawSelector)
+        const keyObj = {
+          s: 'sticky',
+          f: 'fixed',
+          a: 'absolute',
+          r: 'relative',
+          d: 'static',
+        }
+        return `${selector}{position:${keyObj[type]};}`
+      },
+    ],
+    // rotate
+    [
+      /^rot-([\d]+)$/,
+      ([_, num], { rawSelector }) => {
+        const selector = e(rawSelector)
+        return `${selector}{transform:rotate(${num}deg)}`
+      },
+    ],
+  ]
+  //  for microApp
   if (options.type === 'weapp') {
+    // margin padding
     res.push([
       /^(p|m)-{0,1}([xylrtb]{0,1})-{0,1}([\d]+)$/,
       ([_, key, type, num], { rawSelector }) => {
@@ -114,16 +128,15 @@ const getFnRuleList = (options: IOption = {}) => {
           return `${selector} {${keyObj[key]}:${num}${unit};}`
         }
         if (type === 'x') {
-          return `${selector} {${keyObj[key]}:0 ${num}${unit};}`
+          return `${selector} {${keyObj[key]}-left:${num}${unit};${keyObj[key]}-right:${num}${unit};}`
         }
         if (type === 'y') {
-          return `${selector} {${keyObj[key]}:${num}${unit} 0;}`
+          return `${selector} {${keyObj[key]}-top:${num}${unit};${keyObj[key]}-bottom:${num}${unit};}`
         }
         return `${selector}{${keyObj[key]}-${keyObj[type]}:${num}${unit};}`
       },
     ])
   }
-
   return res
 }
 export default presetLj
